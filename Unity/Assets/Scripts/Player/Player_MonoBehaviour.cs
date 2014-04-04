b11ae8e89ef6a115aa5e9e82e7716bd143337d67
Happy Apple <0x20pirate@gmail.com>
2014-04-04 21:45:36 +1100

    Fixed collision and incorporated ground friction.

--- Unity/Assets/Scripts/Player/Player_MonoBehaviour.cs
+++ Unity/Assets/Scripts/Player/Player_MonoBehaviour.cs
@@ -1,5 +1,10 @@
 ﻿public partial class Player
 {
+    private void Awake()
+    {
+        Controller.detectCollisions = true;
+    }
+
     private void Update()
     {
         InputUpdate();
--- Unity/Assets/Scripts/Player/Player_Movement.cs
+++ Unity/Assets/Scripts/Player/Player_Movement.cs
@@ -2,11 +2,16 @@
 
 public partial class Player
 {
-    [SerializeField] private float _runSpeed = 5f;
+    [SerializeField] private float _runSpeed = 45f;
+    [SerializeField] private float _groundFriction = 0.5f;
+    [SerializeField] private float _gravity = 0.981f;
 
     private bool _isClimbing;
 
-    private bool HasCollided { get { return Controller.detectCollisions; }}
+    private bool HasCollided
+    {
+        get { return Controller.collisionFlags != CollisionFlags.None; }
+    }
 
     private bool HasCollidedSides
     {
@@ -23,41 +28,64 @@ public partial class Player
         get { return (Controller.collisionFlags & CollisionFlags.CollidedBelow) != 0; }
     }
 
+    private Vector3 _force = Vector3.zero;
+    private Vector3 _velocity = Vector3.zero;
+
     private void MovementInputEvent(Vector2 input)
     {
-        Vector3 moveVector = Vector3.zero;  
-
         // horizontal movement
         if (!_isClimbing)
         {
             // TODO: Acceleration, inherit platform velocity
-            moveVector.x = input.x*_runSpeed;
+            _force.x = input.x * _runSpeed;
         }
 
         // vertical movement
         // climbing
         // jumping
         // flying
+        // apply gravity here
+        _force.y -= Mathf.Abs(_gravity);
 
         // perform movement here
-        moveVector *= Time.deltaTime;
-        Controller.Move(moveVector);
+        // _acceleration = _force / mass; (assume 1.0f, use force in place of acceleration)
+        _velocity += _force * Time.deltaTime;
+
+        Controller.Move((_velocity - _force * Time.deltaTime / 2f) * Time.deltaTime);
         
         // perform collisions here
         if (!HasCollided) return;
 
+        Debug.Log("Collided!");
+
+        Vector3 undoCollideMovement = _velocity * Time.deltaTime;
+
         if (HasCollidedSides)
         {
-            moveVector.x *= -1f;
+            undoCollideMovement.x *= -1f;
+
+            _velocity.x = 0f;
+            _force.x = 0f;
         }
 
         if (HasCollidedAbove || HasCollidedBelow)
         {
-            moveVector.y *= -1f;
+            undoCollideMovement.y *= -1f;
+
+            // TODO: Flying rules
+            _velocity.y = 0f;
+            _force.y = 0f;
+
+            // apply ground friction here
+            if (HasCollidedBelow)
+            {
+                Debug.Log("Ground");
+                _velocity.x *= _groundFriction;
+            }
         }
         
         // perform adjustment movement
-        transform.Translate(moveVector);
+        transform.Translate(undoCollideMovement);
     }
 
 }
